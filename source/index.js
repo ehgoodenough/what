a6f55607e9932f1ea35b5218fcce2d1c3d953cec
Andrew McPherson <mcphandr@amazon.com>
2017-09-14 18:15:44 -0700

    Moved a little green square across the screen.

--- source/index.js
+++ source/index.js
@@ -1,10 +1,19 @@
 import * as Pixi from "pixi.js"
+import Yaafloop from "yaafloop"
 
 const WIDTH = 640
 const HEIGHT = 360
 
 var renderer = Pixi.autoDetectRenderer({
-  width: WIDTH, height: HEIGHT, transparent: true
+    width: WIDTH, height: HEIGHT, transparent: true
 })
 
 document.getElementById("frame").appendChild(renderer.view)
+
+var texture = Pixi.Texture.from(require("./images/dude.png"))
+var sprite = new Pixi.Sprite(texture)
+
+var loop = new Yaafloop(function(delta) {
+    sprite.position.x += 1 * delta.f
+    renderer.render(sprite)
+})
