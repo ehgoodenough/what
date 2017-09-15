abba0046088e9c005d9c4fdd988f2ee2f5805d12
Andrew McPherson <mcphandr@amazon.com>
2017-09-14 17:59:03 -0700

    Added a canvas. :]

--- source/index.js
+++ source/index.js
@@ -1,1 +1,10 @@
-console.log("HELLO WORLD!! WHAT")
+import * as Pixi from "pixi.js"
+
+const WIDTH = 640
+const HEIGHT = 360
+
+var renderer = Pixi.autoDetectRenderer({
+  width: WIDTH, height: HEIGHT, transparent: true
+})
+
+document.getElementById("frame").appendChild(renderer.view)
